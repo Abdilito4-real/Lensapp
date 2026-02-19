e15f48eef601acd64a630ed024ce45592d56d045
Abdallah Abubakar <abdallahabubakar269@gmail.com>
2026-02-19 14:44:27 +0000

    Access to manifest at 'https://6000-firebase-studio-1771446686631.cluste

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,12 @@
-// A minimal service worker to enable PWA installability.
-self.addEventListener('fetch', () => { return });
+// This is a basic service worker to enable PWA functionality.
+// It doesn't implement any caching strategies yet.
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+});
+
+self.addEventListener('fetch', (event) => {
+  // This service worker doesn't intercept any fetch requests.
+  // It's just here to make the app installable.
+  event.respondWith(fetch(event.request));
+});
