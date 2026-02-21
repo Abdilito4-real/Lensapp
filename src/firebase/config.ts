f273845000abe961f57284e4dccb215520b6049b
Abdallah Abubakar <abdallahabubakar269@gmail.com>
2026-02-21 08:42:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-353420211-db574",
+  "appId": "1:801598603419:web:4ccd7e262a7bd241bc863e",
+  "apiKey": "AIzaSyDVKhUEGjsb1bhhgZjYEGmVRB0bO8ZESdg",
+  "authDomain": "studio-353420211-db574.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "801598603419"
+};
