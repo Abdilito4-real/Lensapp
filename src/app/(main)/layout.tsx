3468d53139490666c2f09ef920cab6773e53ea4f
Abdallah Abubakar <abdallahabubakar269@gmail.com>
2026-02-22 16:18:52 +0000

    Found the issue.
    You created src/app/providers.tsx correctly.
    
    You crea

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { AppHeader } from "@/components/app-header";
 import { BottomNav } from "@/components/bottom-nav";
 import { DesktopNav } from "@/components/desktop-nav";
+import { FirebaseClientProvider } from "@/firebase";
 
 export default function MainLayout({
   children,
